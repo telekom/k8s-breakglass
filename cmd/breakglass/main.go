7dffdcf2e5bcdb2b46ac90fb593b9d895238858f
grro-mobica <grzegorz.roclawski@mobica.com>
2024-07-15 15:23:06 +0200

    init basic webhook functionality

--- cmd/breakglass/main.go
+++ cmd/breakglass/main.go
@@ -1,4 +1,58 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/kubernetes"
+	rest "k8s.io/client-go/rest"
+)
+
+var (
+	universalDeserializer = serializer.NewCodecFactory(runtime.NewScheme()).UniversalDeserializer()
+)
+
+type ServerParameters struct {
+	port     int    // webhook server port
+	certFile string // path to the x509 certificate
+	keyFile  string // path to the x509 private key
+}
+
+var parameters ServerParameters
+
+var config *rest.Config
+var clientSet *kubernetes.Clientset
+
 func main() {
+
+	flag.IntVar(&parameters.port, "port", 8443, "Webhook server port.")
+	flag.StringVar(&parameters.certFile, "tlsCertFile", "/etc/breakglass/certs/tls.crt", "File containing the x509 Certificate for HTTPS.")
+	flag.StringVar(&parameters.keyFile, "tlsKeyFile", "/etc/breakglass/certs/tls.key", "File containing the x509 private key to --tlsCertFile.")
+	flag.Parse()
+
+	// get access to service account token
+	c, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	config = c
+
+	// get the kubernetes client
+	cs, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientSet = cs
+
+	http.HandleFunc("/mutate", HandleMutate)
+	log.Fatal(http.ListenAndServeTLS(":"+strconv.Itoa(parameters.port), parameters.certFile, parameters.keyFile, nil))
+
+}
+
+func HandleMutate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test"))
 }
