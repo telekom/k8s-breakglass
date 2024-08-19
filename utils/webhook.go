3546dcda61a5a13d1b38ac2f46dcf4cb155b0885
grro-mobica <grzegorz.roclawski@mobica.com>
2024-08-19 11:21:59 +0200

    files needed for the webhook

---
+++ utils/webhook.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/kubernetes"
+	rest "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type ServerParameters struct {
+	port     int    // webhook server port
+	certFile string // path to the x509 certificate
+	keyFile  string // path to the x509 private key
+}
+
+var parameters ServerParameters
+
+var (
+	universalDeserializer = serializer.NewCodecFactory(runtime.NewScheme()).UniversalDeserializer()
+)
+
+var config *rest.Config
+var clientSet *kubernetes.Clientset
+
+func main() {
+
+	flag.IntVar(&parameters.port, "port", 8443, "Webhook server port.")
+	flag.StringVar(&parameters.certFile, "tlsCertFile", "/etc/breakglass/certs/tls.crt", "File containing the x509 Certificate for HTTPS.")
+	flag.StringVar(&parameters.keyFile, "tlsKeyFile", "/etc/breakglass/certs/tls.key", "File containing the x509 private key to --tlsCertFile.")
+	flag.Parse()
+
+	kubeconfig := os.Getenv("KUBECONFIG")
+
+	if kubeconfig == "" {
+		// get access to service account token
+		c, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		config = c
+	} else {
+		// get access to cluster using kubeconfig file
+		fmt.Println("kubeconfig path: " + kubeconfig)
+
+		c, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+		config = c
+	}
+
+	// get the kubernetes client
+	cs, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientSet = cs
+
+	http.HandleFunc("/authorize", HandleAuthorize)
+	log.Fatal(http.ListenAndServeTLS(":"+strconv.Itoa(parameters.port), parameters.certFile, parameters.keyFile, nil))
+
+}
+
+func HandleAuthorize(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to read request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	//stdout will be visible in pod logs
+	fmt.Println(string(body))
+
+	w.Header().Set("Content-Type", "application/json")
+	response := `{
+		"apiVersion": "authorization.k8s.io/v1",
+		"kind": "SubjectAccessReview",
+		"status": {
+		  "allowed": false
+		}
+	}`
+	w.Write([]byte(response))
+}
