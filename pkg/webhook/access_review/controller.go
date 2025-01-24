bd3563ec66fc7a01a949d824af8fe2353c7f42c3
bazko1 <bazyli.kot.dev@gmail.com>
2025-01-24 17:36:12 +0100

    update template and add front review url

--- pkg/webhook/access_review/controller.go
+++ pkg/webhook/access_review/controller.go
@@ -169,6 +169,7 @@ func (wc BreakglassSessionController) sendOnRequestEmail(bs v1alpha1.BreakglassS
 			RequestedCluster:  bs.Spec.Cluster,
 			RequestedUsername: bs.Spec.Username,
 			RequestedGroup:    bs.Spec.Group,
+			URL:               fmt.Sprintf("%s/breakglassSession/review?name=%s", wc.config.ClusterAccess.FrontendPage, bs.Name),
 		})
 		if err != nil {
 			wc.log.Errorf("failed to render email template: %v", err)
