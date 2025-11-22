0a23543aa9f57a28b6442a7012d50c43281e281a
Maximilian Rink <maximilian.rink@telekom.de>
2025-11-22 12:11:47 +0100

    Moer UI fixes
    Signed-off-by: Maximilian Rink <maximilian.rink@telekom.de>

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: ["plugin:vue/vue3-recommended", "@vue/eslint-config-typescript/recommended", "@vue/eslint-config-prettier"],
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+  rules: {
+    "vue/hypenize-parameters": "off",
+    "vue/v-on-event-hyphenation": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
