773f18abfd3fdc272e704ed98f3b0cc77409a665
Maximilian Rink <maximilian.rink@telekom.de>
2026-06-20 22:59:52 +0200

    fix: keep mail TLS nosec on insecure field

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -84,9 +84,9 @@ func NewSenderFromMailProvider(mpConfig *config.MailProviderConfig, brandingName
 
 	if mpConfig.InsecureSkipVerify {
 		log.Warnw("InsecureSkipVerify is enabled for mail TLS connection - not recommended for production")
-		d.TLSConfig = &tls.Config{ // #nosec G402 -- explicit mail provider option for test/dev SMTP endpoints
+		d.TLSConfig = &tls.Config{
 			MinVersion:         tls.VersionTLS12,
-			InsecureSkipVerify: true,
+			InsecureSkipVerify: true, // #nosec G402 -- explicit mail provider option for test/dev SMTP endpoints
 		}
 	}
 
