754c8f5e6b3c49c1923cc7bb57e3648ffaa4ea24
Maximilian Rink <maximilian.rink@telekom.de>
2026-06-30 03:57:58 +0200

    Fail closed on missing IdentityProvider in session limits (#1028)
    * Fail closed on missing IdentityProvider in session limits (Closes #968)
    
    * Fix go fmt
    
    * Fix webhook whitespace lint error
    
    * Revert "Fix webhook whitespace lint error"
    
    This reverts commit 34b7f23460342256a7a308205240eb29cc3314ba.
    
    * Address PR 1028 comments and revert bad syntax
    
    * Add test for missing IDP

---
+++ pkg/breakglass/session_controller_status_test.go
@@ -0,0 +1,33 @@
+package breakglass
+
+import (
+	"context"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zaptest"
+	breakglassv1alpha1 "github.com/telekom/k8s-breakglass/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestCheckSessionLimits_MissingIDP(t *testing.T) {
+	log := zaptest.NewLogger(t).Sugar()
+	scheme := runtime.NewScheme()
+	_ = breakglassv1alpha1.AddToScheme(scheme)
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+	
+	wc := &BreakglassSessionController{
+		sessionManager: &SessionManager{
+			Client: fakeClient,
+		},
+	}
+
+	esc := &breakglassv1alpha1.BreakglassEscalation{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-esc"},
+	}
+
+	err := wc.checkSessionLimits(context.Background(), esc, "missing-idp", "test@example.com", []string{"developers"}, log)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "IdentityProvider \"missing-idp\" not found")
+}
